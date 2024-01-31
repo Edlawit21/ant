76b7a37036a82613e899ea7124a10f6c2f74d221
Edlawit21 <edlawitbeyene21@gmail.com>
2024-01-31 13:54:44 +0200

    testing this

--- src/pages/AddStudent.js
+++ src/pages/AddStudent.js
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 function AddStudent() {
-  return (
-    <div>
-      add
-    </div>
-  )
+  return <div>add yes</div>;
 }
 
-export default AddStudent
+export default AddStudent;
